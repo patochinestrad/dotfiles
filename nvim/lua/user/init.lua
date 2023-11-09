befbf60d9fbf0b553ab8cd9d843679755f7f8495
Patricio Chinestrad <patriciochinestrad@gmail.com>
2023-11-09 09:54:32 +0100

    Updated nvim

--- nvim/lua/user/init.lua
+++ nvim/lua/user/init.lua
@@ -5,7 +5,22 @@ return {
     "AstroNvim/astrocommunity",
     { import = "astrocommunity.colorscheme.catppuccin" },
     -- ... import any community contributed plugins here
-  },
+    { import = "astrocommunity.completion.copilot-lua" },
+  { -- further customize the options set by the community
+    "zbirenbaum/copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept = "<C-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-.>",
+          prev = "<C-,>",
+          dismiss = "<C/>",
+        },
+      },
+    },
+  },  },
   {
     "lambdalisue/suda.vim"
   },
